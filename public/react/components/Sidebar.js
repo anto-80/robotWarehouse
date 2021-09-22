783104cf73b622b60fdbc026ccb80839a7414ff9
anto-80 <amwawe@gmail.com>
2021-09-22 10:11:44 -0500

    format

--- public/react/components/Sidebar.js
+++ public/react/components/Sidebar.js
@@ -7,30 +7,30 @@ import {Link} from 'react-router-dom';
     return (
         
 
-<div className="bg-white" id="sidebar-wrapper">
-    <div className="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
-      <i className="fas fa-robot" /> Houston Dynamics
+      <div className="bg-white" id="sidebar-wrapper">
+        <div className="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
+          <i className="fas fa-robot" /> Houston Dynamics
     </div>
-    <div className="list-group list-group-flush my-3">
+        <div className="list-group list-group-flush my-3">
 
-     <Link to="/" className="list-group-item list-group-item-action bg-transparent second-text fw-bold"> 
-        <i className="fas fa-tachometer-alt me-2" />Dashboard
+          <Link to="/" className="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i className="fas fa-tachometer-alt me-2" />Dashboard
       </Link>
 
-      <Link to="/warehouses"  className="list-group-item list-group-item-action bg-transparent second-text fw-bold ">
-        <i className="fas fa-warehouse" /> Warehouses
+          <Link to="/warehouses" className="list-group-item list-group-item-action bg-transparent second-text fw-bold ">
+            <i className="fas fa-warehouse" /> Warehouses
           </Link>
 
-          <Link to="/robots"  className="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-        <i className="fas fa-chart-line me-2" /> Robots
+          <Link to="/robots" className="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i className="fas fa-chart-line me-2" /> Robots
         </Link>
 
-        <Link to="/analytics" className="list-group-item list-group-item-action bg-transparent second-text fw-bold">
-        <i className="fas fa-gift me-2" />Analytics
+          <Link to="/analytics" className="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <i className="fas fa-gift me-2" />Analytics
         </Link>
-    </div>
-  </div>
-        
+        </div>
+      </div>
+
     )
 }
 
