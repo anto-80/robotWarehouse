484dc64b7927f9259a52ef23b9aeb8008b9b3f67
anto-80 <amwawe@gmail.com>
2021-09-23 10:53:50 -0500

    added test and responsiveness

---
+++ public/react/components/Robots.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import  Robots  from './Robots';
+
+describe("Robots component", () => {
+
+  //Snapshot test: Does our Component render as expected?
+
+  test("renders Robots component as expected", () => {
+    const robots = ['R2D2', 'T-800'];
+
+
+    const component = renderer.create(<Robots robots={robots} />)
+
+    const snapshot = component.toJSON();
+
+    console.log("What does our snapshot look like?  ", snapshot)
+
+    expect(snapshot).toMatchSnapshot();
+  })
+
+  //Behavior Test: Does our Component handle data passed in as props?
+
+  test("Contains items passed down as props", () => {
+    const robots = ['R2D2', 'T-800'];
+    
+    const component = renderer.create(<Robots robots={robots}/>);
+    const treeEl = component.toTree()
+
+    
+
+    expect(treeEl.props.robots.length).toBe(2)
+  })
+
+})
